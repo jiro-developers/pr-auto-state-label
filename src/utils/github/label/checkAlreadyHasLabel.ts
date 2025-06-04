0646ce0a1cdf8edcf13d393343a8e6e1fae6fc80
FrontLeejonghun <leejonghun32425@gmail.com>
2025-06-04 14:18:36 +0900

    refactor: 라벨 제거 로직을 개선합니다
    - 라벨 제거 함수 호출 방식을 변경하여 코드 가독성을 높입니다.
    - 불필요한 주석을 제거하여 코드베이스를 정리합니다.

--- src/utils/github/label/checkAlreadyHasLabel.ts
+++ src/utils/github/label/checkAlreadyHasLabel.ts
@@ -3,9 +3,6 @@ interface CheckAlreadyHasLabelProps {
   compareLabel: string; // 비교할 라벨
 }
 
-/**
- *
- * **/
 const checkAlreadyHasLabel = ({ labelList, compareLabel }: CheckAlreadyHasLabelProps) => {
   // 라벨 목록에 비교 라벨이 존재하는지 확인합니다.
   return labelList.some((label) => label === compareLabel);
